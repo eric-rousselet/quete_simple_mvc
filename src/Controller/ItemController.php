da06bc288d722d49022e92366e8cdc2587a0b5bb
Eric Rousselet <rousselet.eric@gmail.com>
2018-10-03 12:57:28 +0200

    Fin mvc3

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -11,4 +11,11 @@ class ItemController
         $items=$itemManager->selectAllItems();
         require __DIR__ . '/../View/item.php';
     }
+    public function show(int $id)
+    {
+        $itemManager = new ItemManager();
+        $item = $itemManager->selectOneItem($id);
+
+        require __DIR__ . '/../View/showItem.php';
+    }
 }
